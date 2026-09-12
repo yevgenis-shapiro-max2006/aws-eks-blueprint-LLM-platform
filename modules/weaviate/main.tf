5cde74720c41efbd186e6b37b105221e44a7f22b
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-12 19:26:25 +0300

    Enhance Weaviate Helm release configuration
    Updated Helm release configuration for Weaviate, increasing timeout and enabling atomic operations. Added replica and pod management settings while removing commented-out authentication settings.

--- modules/weaviate/main.tf
+++ modules/weaviate/main.tf
@@ -1,15 +1,28 @@
 
-
 resource "helm_release" "weaviate" {
   name             = "weaviate"
   namespace        = "weaviate"
   create_namespace = true
-  repository       = "https://weaviate.github.io/weaviate-helm"
-  chart            = "weaviate"
-  version          = "17.5.1"  # latest available chart version
-  timeout          = 600
 
-  # Service types
+  repository = "https://weaviate.github.io/weaviate-helm"
+  chart      = "weaviate"
+  version    = "17.5.1"
+
+  timeout          = 1200
+  atomic           = true
+  cleanup_on_fail  = true
+  wait             = true
+
+  set {
+    name  = "replicas"
+    value = "3"
+  }
+
+  set {
+    name  = "podManagementPolicy"
+    value = "Parallel"
+  }
+
   set {
     name  = "service.type"
     value = "ClusterIP"
@@ -20,35 +33,24 @@ resource "helm_release" "weaviate" {
     value = "ClusterIP"
   }
 
-  # Authentication
-  #set {
-  #  name  = "authentication.apikey.enabled"
-  #  value = "true"
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.allowed_keys[0]"
-  #  value = "\"z3EkNNu8HnEotF2JK2xNlG38xXOtRR6Ns3jEQsM5J3c=\""
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.users[0].username"
-  #  value = "admin"
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.users[0].key"
-  #  value = "\"z3EkNNu8HnEotF2JK2xNlG38xXOtRR6Ns3jEQsM5J3c=\""
-  #}
+  set {
+    name  = "storage.size"
+    value = "10Gi"
+  }
 
   set {
     name  = "authentication.anonymous_access.enabled"
     value = "true"
   }
 
-  # Log level
   set {
     name  = "logLevel"
     value = "info"
   }
+
+  set {
+    name  = "serviceMonitor.enabled"
+    value = "true"
+  }
 }
+
